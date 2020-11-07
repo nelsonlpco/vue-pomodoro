a34da01bedb08bd28f636841f768dffd11da7b6f
Nelson <nelsonlpco@gmail.com>
2020-11-07 00:19:52 -0400

    Updates

---
+++ precache-manifest.9854bc31a17d6f49f46882e3e49667fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcfc6d12f5092945d479",
+    "url": "/css/app.93cb7f18.css"
+  },
+  {
+    "revision": "116a7cd936eb65a97d9900625adfea7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a5f77f3396e57f0857d",
+    "url": "/js/about.277973fb.js"
+  },
+  {
+    "revision": "fcfc6d12f5092945d479",
+    "url": "/js/app.9ab78bef.js"
+  },
+  {
+    "revision": "082041735678cf4950a0",
+    "url": "/js/chunk-vendors.5c363ddc.js"
+  },
+  {
+    "revision": "2a9f90bd0e8d00f646cac734f2dc77ee",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
